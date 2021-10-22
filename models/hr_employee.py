6d8ba53fda2457c8929d03d9b08ba05d35d3f575
Yadier Quesada <yadierq87@gmail.com>
2021-10-21 23:47:07 -0400

    [REMOVE] hr_employee.py

--- models/hr_employee.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class HrEmployee(models.Model):
-    _inherit = 'hr.employee'
-
-    gitlab_user = fields.Char()
-    gitlab_keyword = fields.Char()
-    gitlab_company = fields.Char()
