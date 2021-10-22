080dcfb3d33f921db76a1b86a1d525fcccbd2e5d
Yadier Quesada <yadierq87@gmail.com>
2021-10-21 21:12:22 -0400

    [ADD] gitlab class connection and modify fields in employee

--- models/gitlab.py
+++ models/gitlab.py
@@ -1,220 +1,73 @@
-# Copyright 2017 - 2018 Modoolar <info@modoolar.com>
 # License LGPLv3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html).
+import json
 
-import re
-from odoo import models, fields, http
-from odoo.addons.project_git.utils.utils import urljoin
-
-from dateutil.parser import parse
+import requests as requests
+from odoo import models
 
 TYPE = [('gitlab', 'GitLab')]
 
 
-class GitUser(models.Model):
-    _inherit = 'project.git.user'
+class GitlabConnection(models.Model):
 
-    type = fields.Selection(
-        selection_add=TYPE,
-    )
+    def __init__(self):
+        self.token = 'zTeQqdG9LLAGahSW5e9Y'
+        self.api_url = "https://gitlab.com/api/v4/"
+        self.gh_session = requests.Session()
 
+    def __del__(self):
+        self.gh_session.close()
 
-class GitRepository(models.Model):
-    _inherit = 'project.git.repository'
-
-    type = fields.Selection(
-        selection_add=TYPE,
-    )
-
-    def _secret_visible_for_types(self):
-        types = super(GitRepository, self)._secret_visible_for_types()
-        types.append(TYPE[0][0])
-        return types
-
-
-class GitCommit(models.Model):
-    _inherit = 'project.git.commit'
-
-    type = fields.Selection(
-        selection_add=TYPE,
-    )
-
-
-class GitBranch(models.Model):
-    _inherit = 'project.git.branch'
-
-    type = fields.Selection(
-        selection_add=TYPE,
-    )
-
-
-class GitPayloadParser(models.AbstractModel):
-    _inherit = 'project.git.payload.parser'
-
-    # -------------------------------------------
-    # Header
-    # -------------------------------------------
-    def parse_gitlab_header(self, type, raw_payload):
-        headers = http.request.httprequest.headers
-        action_type = self._map_gitlab_action_type(raw_payload)
-
-        data = {
-            "event": raw_payload['object_kind'],
-            "delivery": raw_payload['checkout_sha'],
-            "action_type": action_type,
-            "action": self._format_action_name(action_type),
-        }
-
-        token = headers.get("X-Gitlab-Token", False)
-        if token:
-            data["token"] = token
-
-        return data
-
-    def _map_gitlab_action_type(self, raw_payload):
-
-        def is_delete_event(evt):
-            after = raw_payload['after']
-            return evt == 'push' and (after.isdigit() and not int(after))
-
-        event = raw_payload['object_kind']
-
-        if event not in self._gitlab_supported_event_types():
+    def get_response_url(self, url):
+        if isinstance(url, str) and self.api_url in url:
+            response_git = self.gh_session.get(url, headers={'PRIVATE-TOKEN': self.token})
+            if response_git.status_code == 404:
+                return False
+            elif response_git.status_code == 200:
+                return json.loads(response_git.text)
+        else:
             return False
 
-        # In case of a push we need to check if we have a delete event
-        if is_delete_event(event):
-            event = 'delete'
+    # GET /groups/:id/
+    def get_info_by_group(self, group_id=""):
+        if isinstance(group_id, str) and group_id != "":
+            url_group = f'{self.api_url}groups/{group_id}'
+            info_group = self.get_response_url(url_group)
+            if isinstance(info_group, dict):
+                data = f'Id:{info_group["id"]} Name:{info_group["name"]}'
+                return dict(group=data)
+            else:
+                return dict(message='404 Group Not Found')
+        else:
+            return dict(message='500 Error')
 
-        return event
+    @staticmethod
+    def _list_issues(issues):
+        list_issues = []
+        for issue in issues:
+            data = f'Id:{issue["iid"]} Name:{issue["title"]} weight:{issue["weight"]}'
+            list_issues.append({"Issue": data})
+        return list_issues
 
-    def _format_action_name(self, action_type):
-        return "git_%s" % (action_type,)
+    # GET /projects/:id/issues
+    def get_project_issues(self, project_id=""):
+        if isinstance(project_id, str) and project_id != "":
+            url_proj = f'{self.api_url}projects/{project_id}/issues'
+            issues = self.get_response_url(url_proj)
+            if isinstance(issues, list) and len(issues) > 0:
+                return self._list_issues(issues)
+            else:
+                return dict(message='404 Project Not Found')
+        else:
+            return dict(message='500 Error')
 
-    def _gitlab_supported_event_types(self):
-        return ["push"]
-
-    # -------------------------------------------
-    # Payload
-    # -------------------------------------------
-    def parse_gitlab_payload(self, context):
-        method_name = "parse_gitlab_%s" % context.action_type
-        parse_event_method = getattr(self, method_name)
-        return parse_event_method(context)
-
-    # -------------------------------------------
-    # Action Push
-    # -------------------------------------------
-    def parse_gitlab_push(self, context):
-        return {
-            "repository": self.parse_gitlab_repository(context),
-            "branches": [self.parse_gitlab_branch(context)],
-            "sender": self.parse_gitlab_sender(context)
-        }
-
-    # -------------------------------------------
-    # Action Delete
-    # -------------------------------------------
-    def parse_gitlab_delete(self, context):
-        return {
-            "branches": [self.parse_gitlab_branch(context, False)]
-        }
-
-    # -------------------------------------------
-    # Paring methods
-    # -------------------------------------------
-    def parse_gitlab_branch(self, context, commits=True):
-        payload = context.raw_payload
-        branch_name = payload["ref"].split('/')[-1]
-        data = {
-            "name": branch_name,
-            "type": context.type,
-            "repository_id": context.repository.id,
-            "url": urljoin(
-                payload['project']['homepage'] + '/', 'tree', branch_name
-            ),
-        }
-
-        if commits:
-            data["commits"] = self.parse_gitlab_commits(context)
-
-        return data
-
-    def parse_gitlab_commits(self, context):
-        commits = []
-        for commit in context.raw_payload["commits"]:
-            commits.append(self.parse_gitlab_commit(context, commit))
-        return commits
-
-    def parse_gitlab_commit(self, context, commit):
-        return {
-            "name": commit["id"][:8],
-            "message": commit["message"],
-            "url": commit["url"],
-            "date": fields.Datetime.to_string(parse(commit["timestamp"])),
-            "type": context.type,
-            "author": self.parse_gitlab_commit_author(context, commit),
-        }
-
-    def parse_gitlab_commit_author(self, context, commit):
-        author_data = dict(
-            name=commit["author"]["name"],
-            email=commit["author"]["email"],
-            type=context.type,
-        )
-
-        repository_owner = self.parse_gitlab_repository_owner(context)
-        if repository_owner['email'] == author_data['email']:
-            return repository_owner
-
-        return author_data
-
-    def parse_gitlab_sender(self, context):
-        raw_payload = context.raw_payload
-
-        return {
-            "name": raw_payload["user_name"],
-            "username": raw_payload["user_username"],
-            "email": raw_payload["user_email"],
-            "uuid": raw_payload["user_id"],
-            "avatar": raw_payload["user_avatar"],
-            "type": context.type,
-        }
-
-    def parse_gitlab_repository(self, context):
-        project = context.raw_payload["project"]
-        return {
-            "name": project["name"],
-            "full_name": project["path_with_namespace"],
-            "url": project['homepage'],
-            "type": context.type,
-            "owner": self.parse_gitlab_repository_owner(context)
-        }
-
-    def parse_gitlab_repository_owner(self, context):
-        raw_payload = context.raw_payload
-
-        utils = re.search(
-            r"(https?://.+/)(.+)/.+",
-            raw_payload["repository"]["git_http_url"]
-        )
-
-        link = utils.group(1)
-        username = utils.group(2)
-
-        data = {
-            "name": username.title(),
-            "username": username,
-            "uuid": "",
-            "avatar": "",
-            "url": urljoin(link, username),
-            "email": "",
-            "type": context.type,
-        }
-
-        if username == raw_payload["user_username"]:
-            data["name"] = raw_payload["user_name"]
-            data["uuid"] = raw_payload["user_id"]
-            data["avatar"] = urljoin(link, raw_payload["user_avatar"])
-            data["email"] = raw_payload["user_email"]
-
-        return data
+    # GET /projects/:id/issues?assignee_username=Quesada87  filter by name
+    def get_issues_by_username(self, project_id="", username=""):
+        if project_id == "" or username == "":
+            return dict(message='500 Error')
+        else:
+            url_username = f'{self.api_url}projects/{project_id}/issues?assignee_username={username}'
+            issues = self.get_response_url(url_username)
+            if isinstance(issues, list) and len(issues) > 0:
+                return self._list_issues(issues)
+            else:
+                return dict(message='404 Resource Not Found')
